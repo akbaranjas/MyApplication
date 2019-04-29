4917466d469466a624b80cb9790f5541d6e0a58b
akbaranjas <akbaranjas@gmail.com>
2019-04-29 22:39:26 +0700

    Initial Commit

---
+++ app/src/main/java/ahafidz/com/myapplication/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package ahafidz.com.myapplication.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ahafidz.com.myapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/ahafidz/com/myapplication/activity/SplashActivity.java
@@ -0,0 +1,29 @@
+package ahafidz.com.myapplication.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+
+import ahafidz.com.myapplication.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
