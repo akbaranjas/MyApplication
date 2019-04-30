ea988b97e7cc175f57b918bf22c0b2c12b98fe05
akbaranjas <akbaranjas@gmail.com>
2019-04-30 07:06:54 +0700

    Initial Commit

--- app/src/main/java/ahafidz/com/myapplication/activity/LoginActivity.java
+++ app/src/main/java/ahafidz/com/myapplication/activity/LoginActivity.java
@@ -1,15 +1,107 @@
 package ahafidz.com.myapplication.activity;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import ahafidz.com.myapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText textEmail;
+    private EditText textPassword;
+    private Button loginButton;
+    private static String TAG = "LoginActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        textEmail = findViewById(R.id.input_email);
+        textPassword = findViewById(R.id.input_password);
+        loginButton = findViewById(R.id.btn_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = textEmail.getText().toString();
+        String password = textPassword.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        loginButton.setEnabled(true);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = textEmail.getText().toString();
+        String password = textPassword.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            textEmail.setError("enter a valid email address");
+            valid = false;
+        } else {
+            textEmail.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            textPassword.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            textPassword.setError(null);
+        }
+
+        return valid;
     }
 }
